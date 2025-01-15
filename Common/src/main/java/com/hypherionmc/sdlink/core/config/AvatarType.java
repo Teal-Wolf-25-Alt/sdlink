082df6b400d5dca9625474c3bc2f4f1e78176d1e
Teal <155310651+Teal-Wolf-25-Alt@users.noreply.github.com>
2025-01-15 02:35:45 -0500

    More types

--- Common/src/main/java/com/hypherionmc/sdlink/core/config/AvatarType.java
+++ Common/src/main/java/com/hypherionmc/sdlink/core/config/AvatarType.java
@@ -9,9 +9,21 @@ package com.hypherionmc.sdlink.core.config;
  * The type of User Icon/Avatar that will be used for Discord Messages
  */
 public enum AvatarType {
-    AVATAR("https://mc-heads.net/avatar/{uuid}/512"),
-    HEAD("https://mc-heads.net/head/{uuid}/512"),
-    BODY("https://mc-heads.net/body/{uuid}"),
+    FACE("https://vzge.me/face/512/{uuid}?no=ears"),
+    FACE_EARS("https://vzge.me/face/512/{uuid}"),
+    FRONT("https://vzge.me/front/256/{uuid}?no=ears"),
+    FRONT_EARS("https://vzge.me/front/256/{uuid}"),
+    FRONT_FULL("https://vzge.me/frontfull/384/{uuid}?no=ears"),
+    FRONT_FULL_EARS("https://vzge.me/frontfull/384/{uuid}"),
+    HEAD("https://vzge.me/head/256/{uuid}?no=ears"),
+    HEAD_EARS("https://vzge.me/head/256/{uuid}"),
+    BUST("https://vzge.me/bust/256/{uuid}?no=ears"),
+    BUST_EARS("https://vzge.me/bust/256/{uuid}"),
+    FULL("https://vzge.me/full/256/{uuid}?no=ears"),
+    FULL_EARS("https://vzge.me/full/256/{uuid}"),
+    /*AVATAR removed, use FACE instead*/
+    MC_HEAD("https://mc-heads.net/head/{uuid}/512"),
+    PLAYER("https://mc-heads.net/player/{uuid}/512"),
     COMBO("https://mc-heads.net/combo/{uuid}/512");
 
     private final String url;
